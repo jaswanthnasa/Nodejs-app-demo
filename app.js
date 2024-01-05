1c80698d699072aca57c035c1bbbbe2f59a2f8aa
srebreni3 <ahmedsrebrenica33@gmail.com>
2024-01-05 22:57:03 +0100

    modified app.js file

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, friend!!!\n');
+  res.setHeader('Content-Type', 'text/html'); //
+  res.write('<h1>Hello, friend!</h1>\n'); 
+  res.end();
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
