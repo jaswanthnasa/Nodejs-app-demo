2978b22074c00ed997805b163649f4c5d2e62d4d
srebreni3 <ahmedsrebrenica33@gmail.com>
2024-01-05 23:04:29 +0100

    modified app.js file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html'); //
-  res.write('<h1>Hello, friend!</h1>\n'); 
+  res.write('<h1>Hello, friend...</h1>\n'); 
   res.end();
 });
 
