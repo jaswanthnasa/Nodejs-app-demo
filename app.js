81cb9e3db26691288d9372e7e93be1e9317e7350
jaswanth <jas@example.com>
2024-06-13 15:04:12 +0530

    udpated app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html'); //
-  res.write('<h1>Hello Friend, this is first update!</h1>\n'); 
+  res.write('<h1>Hello world. </h1>\n'); 
+  res.write('<h2> This is a sample node js application depoyed in AWS EC2!!</h2>\n'); 
   res.end();
 });
 
